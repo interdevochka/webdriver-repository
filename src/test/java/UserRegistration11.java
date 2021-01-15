0238c7392705edfd3823c6352195bc66f4a9a2f7
interdevochka <stitch_girl@mail.ru>
2021-01-15 13:16:19 +0700

    Update UserRegistration11.java

--- src/test/java/UserRegistration11.java
+++ src/test/java/UserRegistration11.java
@@ -30,27 +30,28 @@ public class UserRegistration11 {
         }
 
         @Test
-        public void registration() throws InterruptedException {
+        public void rega() throws InterruptedException {
 
             String password = "root";
             String email = randomEmail();
 
-
-            driver.get("http://localhost/litecart/en/");
+            // Кликает по ссылке добавить нового пользователя
+            driver.get("http://localhost/litecart/public_html/en/");
             WebElement leftBlock = driver.findElement(By.name("login_form"));
             List<WebElement> trList = leftBlock.findElements(cssSelector("tr"));
             WebElement tr = trList.get(trList.size()-1);
-            WebElement a = tr.findElement(By.name("a"));
+            WebElement a = tr.findElement(By.tagName("a"));
             a.click();
-
+            Thread.sleep(2000);
             // next page
-            WebElement create_account_block = driver.findElement(By.className("content"));  //Блок регистрации
+            WebElement create_account_block = driver.findElement(By.id("create-account"));  //Блок регистрации
 
             WebElement first_name = create_account_block.findElement(By.name("firstname"));
             first_name.sendKeys("Vasya"); //кликаем и заполняем поле first name
 
             WebElement last_name = create_account_block.findElement(By.name("lastname"));
             last_name.sendKeys("Pupkin"); //кликаем и заполняем поле last name
+
 
             WebElement address_1 = create_account_block.findElement(By.name("address1"));
             address_1.sendKeys("Avenue str. 10-2"); //кликаем и заполняем поле address1
@@ -61,28 +62,51 @@ public class UserRegistration11 {
             WebElement city = create_account_block.findElement(By.name("city"));
             city.sendKeys("Mobile"); //кликаем и заполняем поле city
 
-            WebElement country = create_account_block.findElement(By.id("select2-country_code-nw-container"));
+            WebElement country = driver.findElement(By.className("select2-selection__arrow"));
             country.click();
-            country.sendKeys("United States"+ Keys.ENTER); //кликаем на выпадающий список  и прописываем в поле country Unated States
+            WebElement country2 = driver.findElement(By.className("select2-search__field"));
+            country2.sendKeys("United States" + Keys.ENTER);
 
-            WebElement zone = driver.findElement(By.name("zone_code"));
+            WebElement zone = driver.findElement(By.cssSelector("select[name='zone_code']"));
+            zone.click();
             Select select = new Select(zone);
-            select.selectByVisibleText("Alabama");
+            Thread.sleep(1000);
+            select.selectByVisibleText("Alaska");
 
-            WebElement phone = create_account_block.findElement(By.id("phone"));
+            WebElement emailField = create_account_block.findElement(By.cssSelector("input[name='email']"));
+            emailField.sendKeys(email); //кликаем и заполняем поле email
+
+            WebElement phone = create_account_block.findElement(By.cssSelector("input[name='phone']"));
             phone.clear();
             phone.sendKeys("323332233"); //кликаем и заполняем поле phone
 
+            WebElement desired_password = create_account_block.findElement(By.name("password"));
+            desired_password.sendKeys(password); //кликаем и заполняем поле desired password
+
+            WebElement confirm_password = create_account_block.findElement(By.name("confirmed_password"));
+            confirm_password.sendKeys(password); //кликаем и заполняем поле confirm password
 
             WebElement create_account_button = create_account_block.findElement(By.name("create_account"));
             create_account_button.click(); //кликаем по кнопке create account
+            Thread.sleep(2000);
 
+            // разлогиниться + переход на главную страницу
+            WebElement link = driver.findElement(By.linkText("Logout"));
+            link.click();
+            Thread.sleep(2000);
 
+            // заново залогиниться
+            WebElement emailAddress = driver.findElement(By.name("email"));
+            emailAddress.sendKeys(email);
 
+            WebElement password_main_page = driver.findElement(By.cssSelector("input[name='password']"));
+            password_main_page.clear();
+            password_main_page.sendKeys(password + Keys.ENTER); //кликаем и заполняем password
+            Thread.sleep(2000);
 
-
-
-
+            // ещё раз разлогиниться
+            WebElement finalOut = driver.findElement(By.linkText("Logout"));
+            finalOut.click();
 
         }
 
